053cbb92e049177adff896abc24967aafbf56f6f
Sergey <s.gaykov@yandex.ru>
2015-04-14 18:20:30 +0300

    Spaces fix

--- bootstrap-tabcollapse.js
+++ bootstrap-tabcollapse.js
@@ -59,29 +59,29 @@
 
         $panelHeadings.each(function() {
             var $panelHeading = $(this),
-                $parentLi = $panelHeading.data('bs.tabcollapse.parentLi');
+            $parentLi = $panelHeading.data('bs.tabcollapse.parentLi');
 
             var $oldHeading = view._panelHeadingToTabHeading($panelHeading);
 
             $parentLi.removeClass('active');
-			if ($parentLi.parent().hasClass('dropdown-menu') && !$parentLi.siblings('li').hasClass('active')) {
-				$parentLi.parent().parent().removeClass('active');
-			}
+            if ($parentLi.parent().hasClass('dropdown-menu') && !$parentLi.siblings('li').hasClass('active')) {
+                $parentLi.parent().parent().removeClass('active');
+            }
 
-			if (!$oldHeading.hasClass('collapsed')) {
-				$parentLi.addClass('active');
-				if ($parentLi.parent().hasClass('dropdown-menu')) {
-					$parentLi.parent().parent().addClass('active');
-				}
-			} else {
-				$oldHeading.removeClass('collapsed');
-			}
-            
+            if (!$oldHeading.hasClass('collapsed')) {
+                $parentLi.addClass('active');
+                if ($parentLi.parent().hasClass('dropdown-menu')) {
+                    $parentLi.parent().parent().addClass('active');
+                }
+            } else {
+                $oldHeading.removeClass('collapsed');
+            }
+
             $parentLi.append($panelHeading);
         });
 
         if (!$('li').hasClass('active')) {
-        	$('li').first().addClass('active')
+            $('li').first().addClass('active')
         }
 
         var $panelBodies = this.$accordion.find('.js-tabcollapse-panel-body');
